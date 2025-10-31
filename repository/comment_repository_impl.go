7a6e0c0ae0abd7c8124abb1e4eae8bca24293894
faz379 <fazelianpramudia@gmail.com>
2025-10-31 22:41:22 +0700

    fis comment

--- repository/comment_repository_impl.go
+++ repository/comment_repository_impl.go
@@ -14,11 +14,16 @@ func NewCommentRepository() CommentRepository {
 }
 
 func (repository CommentRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, comment domain.Comment) domain.Comment {
-	sql := "INSERT INTO comments(content, post_id, author_id, created_at) VALUES (?, ?, ?, ?)"
+	sql := "INSERT INTO comments(content, post_id, author_id) VALUES (?, ?, ?)"
 	result, err := tx.ExecContext(ctx, sql, comment.Content, comment.PostId, comment.AuthorId, comment.CreatedAt)
 	helper.PanicIfError(err)
 
 	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	query := "SELECT id, content, post_id, author_id, created_at FROM comments WHERE id = ?"
+	row := tx.QueryRowContext(ctx, query, id)
+	err = row.Scan(&comment.Id, &comment.Content, &comment.PostId, &comment.AuthorId, &comment.CreatedAt)
 	helper.PanicIfError(err)
 
 	comment.Id = int(id)
