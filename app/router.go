ff5c00b71af4e1059537cbca136d04a71576dfef
faz379 <fazelianpramudia@gmail.com>
2025-11-14 19:00:49 +0700

    route

--- app/router.go
+++ app/router.go
@@ -27,7 +27,7 @@ func NewRouter(postController controller.PostController, userController *control
 
 	router.GET("/api/posts", postController.FindAll)
 	//router.GET("/api/posts/:postId", postController.FindById)
-	router.POST("/api/posts", postController.Create)
+	router.POST("/api/post", postController.Create)
 	router.PUT("/api/posts/:postId", postController.Update)
 	router.DELETE("/api/posts/:postId", postController.Delete)
 	router.GET("/api/posts/:slug", postController.FindBySlug)
